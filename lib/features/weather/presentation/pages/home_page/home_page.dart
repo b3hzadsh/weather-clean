4adae17eec29dbaa809148822c012da0ddda32a0
Behzad Shabanifard <b3hzadsh@gmail.com>
2023-06-20 08:58:17 +0330

    init with old app code

---
+++ lib/features/weather/presentation/pages/home_page/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class HomePage extends StatelessWidget {
+  /// IN this page we will show to user name of choosen city and
+  /// the temprature for that city 
+  /// its enough for now 
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
