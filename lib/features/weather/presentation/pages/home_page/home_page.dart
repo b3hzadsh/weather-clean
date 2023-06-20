ae35b7544d602f41b7cbcc4f121b5bc481242434
Behzad Shabanifard <b3hzadsh@gmail.com>
2023-06-20 09:04:07 +0330

    make the home page fake data

--- lib/features/weather/presentation/pages/home_page/home_page.dart
+++ lib/features/weather/presentation/pages/home_page/home_page.dart
@@ -1,14 +1,25 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   /// IN this page we will show to user name of choosen city and
-  /// the temprature for that city 
-  /// its enough for now 
+  /// the temprature for that city
+  /// its enough for now
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: const <Widget>[
+          SizedBox(height: 50),
+          Text('place holder for city name, e.x tehran'),
+          Text('temp : 25 C'),
+
+        ],
+      ),
+    );
   }
 }
